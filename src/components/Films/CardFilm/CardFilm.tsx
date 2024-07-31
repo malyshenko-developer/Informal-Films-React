9e93e151f95eacb698b43f7f61ffa36cc7cea7f8
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2024-07-31 12:57:48 +1000

    fix #3

--- src/components/Films/CardFilm/CardFilm.tsx
+++ src/components/Films/CardFilm/CardFilm.tsx
@@ -21,7 +21,7 @@ function CardFilm({ name, image, reting, id }: CardFilmsProps) {
     return (
         <Paper sx={{width: '296px'}} elevation={10}>
             <Card>
-                <Link to={`Informal-Films-React/film/${id}`}>
+                <Link to={`film/${id}`}>
                     <CardActionArea>
                         {
                             isLoad && (
